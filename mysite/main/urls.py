a82904facd98606409f23235b606037803e5f77a
pranatsharma1 <sharmapranat123@gmail.com>
2019-09-09 11:52:18 +0530

    Login and Logout added and customized the webpages

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -19,5 +19,8 @@ from . import views
 app_name="main"
 
 urlpatterns = [
-   path("",views.homepage,name="homepage"),    
+   path("",views.homepage,name="homepage"),
+   path("register/", views.register, name="register"), 
+   path("logout/", views.logout_request, name="logout"), 
+   path("login/", views.login_request, name="login"), 
 ]
