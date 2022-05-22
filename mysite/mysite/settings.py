e1badafb371ad9e6487be38b2d14335648e3243a
Pranat Sharma <sharmapranat123@gmail.com>
2022-05-22 21:44:11 +0530

    Changee gmail password

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -169,7 +169,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'siinternpedia@gmail.com'
 EMAIL_HOST_PASSWORD = 'bylgannthobcumid'
-# EMAIL_HOST_PASSWORD = 'Imtheguy@285'
+# EMAIL_HOST_PASSWORD = 'Imtheguy@285' 
 EMAIL_PORT = 587
 
 # django_heroku.settings(locals())
